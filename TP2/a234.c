58323695344061ccc01e9c521c3dd793bc69aca0
SOLER Lilian <soler.lilian64@gmail.com>
2022-04-05 14:37:45 +0200

    TP2 - nb_cle, cle_min/max non fonctionnel

--- TP2/a234.c
+++ TP2/a234.c
@@ -4,137 +4,222 @@
 
 #include "a234.h"
 
-#define max(a,b) ((a)>(b)?(a):(b))
+#define max(a, b) ((a) > (b) ? (a) : (b))
 
-
-int hauteur (Arbre234 a)
+int hauteur(Arbre234 a)
 {
-  int h0, h1, h2, h3 ;
-  
+  int h0, h1, h2, h3;
+
   if (a == NULL)
-    return 0 ;
+    return 0;
 
   if (a->t == 0)
-    return 0 ;
+    return 0;
 
-  h0 = hauteur (a->fils [0]) ;
-  h1 = hauteur (a->fils [1]) ;
-  h2 = hauteur (a->fils [2]) ;
-  h3 = hauteur (a->fils [3]) ;
+  h0 = hauteur(a->fils[0]);
+  h1 = hauteur(a->fils[1]);
+  h2 = hauteur(a->fils[2]);
+  h3 = hauteur(a->fils[3]);
 
-  return 1 + max (max (h0,h1),max (h2,h3)) ;
-} 
+  return 1 + max(max(h0, h1), max(h2, h3));
+}
 
-int NombreCles (Arbre234 a)
+int NombreCles(Arbre234 a)
 {
   /*
-     Retourne le nombre de cles presentes dans l'arbre a
+  Retourne le nombre de cles presentes dans l'arbre a
   */
+
+  if (a == NULL)
+    return 0;
+
+  switch (a->t)
+  {
+  case 0:
+    return 0;
+  case 2:
+    return 1 + NombreCles(a->fils[0]) + NombreCles(a->fils[1]);
+  case 3:
+    return 2 + NombreCles(a->fils[0]) + NombreCles(a->fils[1]) + NombreCles(a->fils[2]);
+  case 4:
+    return 3 + NombreCles(a->fils[0]) + NombreCles(a->fils[1]) + NombreCles(a->fils[2]) + NombreCles(a->fils[3]);
+  default:
+    return 0;
+  }
+}
+
+int CleMax(Arbre234 a)
+{
+  /*
+  Retourne plus grande cle de l'arbre a
+  */
+  while (a->fils[0] != NULL){
+    a = a->fils[NombreCles(a)];
+  }
+  return a->cles[NombreCles(a)];
+}
+
+int CleMin(Arbre234 a)
+{
   
-  return 0 ;
-}
-
-int CleMax (Arbre234 a)
-{
   /*
-     plus grande cle de l'arbre a
+  Retourne plus petite cle de l'arbre a
   */
-  
-  return 0 ;
-}
-
-int CleMin (Arbre234 a)
-{
+ /* while (a->fils[0] != NULL)
+  {
+    a = a->fils[0];
+    printf("%d\n", a->cles[0]);
+    afficher_arbre(a, 0);
+  }
+  printf("END WHILE\n");
+  return a->cles[0];*/
   /*
-     Retourne plus petite cle de l'arbre a
-  */
+  int pos ;
+  int x=0;
+  if (A ==NULL )
+    return 0;
+  else {
+    pos = position (x, A) ;
+    if ( (x == A->cles [ pos ] ) )
+      return 1;
+    else
+      return RechercheCle (x+1, A->fils [ pos ] ) ;
+}
+}
+int position (int x; Arbre234 a){}
+/* plus grand pos tel que A->cles[pos]
+x */
+/*
+int pos, trouve =0;
+for (pos=0; pos<a->n && !trouve; pos++){
+  trouve = (x<= a->cles[pos]);
+}
+return pos;
+*/
 
-  return 0 ;
+return 0;
 }
 
-Arbre234 RechercherCle (Arbre234 a, int cle)
+
+Arbre234 RechercherCle(Arbre234 a, int cle)
 {
   /*
      rechercher si la cle a est presente dans
      l'arbre a. Si oui, retourne le noeud ou se trouve la cle.
   */
 
-  return NULL ;
+  return NULL;
 }
 
-void AnalyseStructureArbre (Arbre234 a, int *feuilles, int *noeud2, int *noeud3, int *noeud4)
+void AnalyseStructureArbre(Arbre234 a, int *feuilles, int *noeud2, int *noeud3, int *noeud4)
 {
-  /* 
+  /*
      calculer le nombre de feuilles, de 2-noeuds, 3-noeuds,et 4-noeuds
   */
 }
 
-Arbre234 noeud_max (Arbre234 a)
+Arbre234 noeud_max(Arbre234 a)
 {
   /*
     Retourne le noeud avec la somme maximale des cles internes
   */
-  
-  return NULL ;
+
+  return NULL;
 }
 
-
-void Afficher_Cles_Largeur (Arbre234 a)
+void Afficher_Cles_Largeur(Arbre234 a)
 {
   /*
     Afficher le cles de l'arbre a avec
     un parcours en largeur
   */
 
-  return ;
+  return;
 }
 
-void Affichage_Cles_Triees_Recursive (Arbre234 a)
+void Affichage_Cles_Triees_Recursive(Arbre234 a)
 {
-  /* 
+  /*
      Afficher les cles en ordre croissant
      Cette fonction sera recursive
   */
-     
 }
 
-void Affichage_Cles_Triees_NonRecursive (Arbre234 a)
+void Affichage_Cles_Triees_NonRecursive(Arbre234 a)
 {
-    /* 
-     Afficher les cles en ordre croissant
-     Cette fonction ne sera pas recursive
-     Utiliser une pile
-  */
-
+  /*
+   Afficher les cles en ordre croissant
+   Cette fonction ne sera pas recursive
+   Utiliser une pile
+*/
 }
 
-
-void Detruire_Cle (Arbre234 *a, int cle)
+void Detruire_Cle(Arbre234 *a, int cle)
 {
   /*
     retirer la cle de l'arbre a
   */
 
-  return ;
+  return;
 }
 
-
-
-
-int main (int argc, char **argv)
+void basicMain(Arbre234 a)
 {
-  Arbre234 a ;
+  printf("==== Afficher arbre ====\n");
 
+  afficher_arbre(a, 0);
+
+  printf("\n");
+}
+
+void test_nb_cle(Arbre234 a)
+{
+  printf("==== Test nombre cles ====\n");
+  int nb_cle = NombreCles(a);
+  if (nb_cle != 17)
+  {
+    printf("Erreur NombreCles\n");
+  }
+  printf("Nombre de cles : %d\n", nb_cle);
+  printf("\nDone\n\n");
+}
+
+void test_cle_min(Arbre234 a)
+{
+  printf("==== Test cle min ====\n");
+  int cle_min = CleMin(a);
+  if (cle_min != 1)
+  {
+    printf("Erreur CleMin\n");
+  }
+  printf("Cle min : %d\n", cle_min);
+  printf("\nDone\n\n");
+}
+
+void test_cle_max(Arbre234 a)
+{
+  printf("==== Test cle max ====\n");
+  int cle_max = CleMax(a);
+  if (cle_max != 50)
+  {
+    printf("Erreur CleMax\n");
+  }
+  printf("Cle max : %d\n", cle_max);
+  printf("\nDone\n\n");
+}
+
+int main(int argc, char **argv)
+{
+  Arbre234 a = NULL;
   if (argc != 2)
-    {
-      fprintf (stderr, "il manque le parametre nom de fichier\n") ;
-      exit (-1) ;
-    }
+  {
+    fprintf(stderr, "il manque le parametre nom de fichier\n");
+    exit(-1);
+  }
+  a = lire_arbre(argv[1]);
 
-  a = lire_arbre (argv [1]) ;
-
-  printf ("==== Afficher arbre ====\n") ;
-  
-  afficher_arbre (a, 0) ;
-
+  basicMain(a);
+  test_nb_cle(a);
+  test_cle_min(a);
+  test_cle_max(a);
 }
