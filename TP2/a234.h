a7b0cc29673fa33914a6cf029cc178d7590c5e40
Pierrick Catteau <pierrick.catteau@etu.univ-grenoble-alpes.fr>
2022-04-26 23:49:11 +0200

    Ajout des prototypes

--- TP2/a234.h
+++ TP2/a234.h
@@ -21,4 +21,25 @@ void afficher_arbre (Arbre234 a, int niveau) ;
 
 void ajouter_cle (Arbre234 *a, int cle, int niveau, Arbre234 pere) ;
 
-  
+/* Prototypes des fonctions implémentées durant le TP */
+/* Les fonctions prototypées ici sont celles qui sont opérationnelles */
+
+int hauteur (Arbre234 a);
+
+int NombreCles (Arbre234 a);
+
+int CleMax (Arbre234 a);
+
+int CleMin (Arbre234 a);
+
+Arbre234 RechercherCle (Arbre234 a, int cle);
+
+void AnalyseStructureArbre (Arbre234 a, int *feuilles, int *noeud2, int *noeud3, int *noeud4);
+
+int sommeNoeud(Arbre234 a);
+
+Arbre234 noeud_max (Arbre234 a);
+
+void Afficher_Cles_Largeur (Arbre234 a);
+
+void Affichage_Cles_Triees_Recursive (Arbre234 a);
